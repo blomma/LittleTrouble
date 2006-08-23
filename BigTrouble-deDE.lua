d1dc75b792989e29c88e0eda6b8e81d457e08b90
fleetfoot <fleetfoot@48e1f902-e210-0410-a839-a0138e9d5bd5>
2006-08-23 14:46:24 +0000

    trunk/BigTrouble: * Added a german translation thx to tumetom
    git-svn-id: http://dev.wowace.com/wowace/trunk/BigTrouble@8805 48e1f902-e210-0410-a839-a0138e9d5bd5

---
+++ BigTrouble-deDE.lua
@@ -0,0 +1,16 @@
+--[[
+	German translation thx to tumetom on wowace(irc.freenode.net)
+--]]
+
+local L = AceLibrary("AceLocale-2.0"):new("BigTrouble")
+L:RegisterTranslations("deDE", function()
+return {
+	["Aimed Shot"] = "Gezielter Schuss",
+	["Failed"] = "Fehlgeschlagen",
+	["Interrupted"] = "Unterbrochen",
+	["Rapid Fire"] = "Schnellfeuer",
+	["Quick Shots"] = "Schnelle Schüsse",
+	["Berserking"] = "Berserker",
+	["Auto Shot"] = "Automatischer Schuss",
+}
+end)
--- BigTrouble-enUS.lua
+++ BigTrouble-enUS.lua
@@ -1,12 +1,12 @@
 local L = AceLibrary("AceLocale-2.0"):new("BigTrouble")
 L:RegisterTranslations("enUS", function()
-    return {
-        ["Aimed Shot"] = true,
-        ["Failed"] = true,
-        ["Interrupted"] = true,
-        ["Rapid Fire"] = true,
-        ["Quick Shots"] = true,
-        ["Berserking"] = true,
-        ["Auto Shot"] = true,
-    }
+return {
+	["Aimed Shot"] = true,
+	["Failed"] = true,
+	["Interrupted"] = true,
+	["Rapid Fire"] = true,
+	["Quick Shots"] = true,
+	["Berserking"] = true,
+	["Auto Shot"] = true,
+}
 end)
