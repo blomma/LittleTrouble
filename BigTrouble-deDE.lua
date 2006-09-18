6d02e78fc0799d42a66b144ae4320c89e3962b1a
fleetfoot <fleetfoot@48e1f902-e210-0410-a839-a0138e9d5bd5>
2006-09-18 12:09:20 +0000

    trunk/BigTrouble: * Updated translations
    git-svn-id: http://dev.wowace.com/wowace/trunk/BigTrouble@11246 48e1f902-e210-0410-a839-a0138e9d5bd5

--- BigTrouble-deDE.lua
+++ BigTrouble-deDE.lua
@@ -6,10 +6,7 @@ local L = AceLibrary("AceLocale-2.0"):new("BigTrouble")
 L:RegisterTranslations("deDE", function()
 return {
 	["Aimed Shot"] = "Gezielter Schuss",
-	["Rapid Fire"] = "Schnellfeuer",
-	["Quick Shots"] = "Schnelle Schüsse",
-	["Berserking"] = "Berserker",
-	["Auto Shot"] = "Automatischer Schuss",
+	["Auto Shot"] = "Autom. Schuss",
 	["SPELLCAST_FAILED"] = "Fehlgeschlagen",
 	["SPELLCAST_INTERRUPTED"] = "Unterbrochen",
 }
--- BigTrouble-enUS.lua
+++ BigTrouble-enUS.lua
@@ -2,9 +2,6 @@ local L = AceLibrary("AceLocale-2.0"):new("BigTrouble")
 L:RegisterTranslations("enUS", function()
 return {
 	["Aimed Shot"] = true,
-	["Rapid Fire"] = true,
-	["Quick Shots"] = true,
-	["Berserking"] = true,
 	["Auto Shot"] = true,
 	["SPELLCAST_FAILED"] = "Failed",
 	["SPELLCAST_INTERRUPTED"] = "Interrupted",
